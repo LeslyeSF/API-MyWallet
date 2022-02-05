f239842b1244748bd9c01e850d1420893e0bad2c
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-02-05 01:43:48 -0300

    feat: routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import Joi from "joi";
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import dayjs from "dayjs";
 
 const server = express();
 server.use(cors());
@@ -26,6 +27,12 @@ const userSchema = Joi.object({
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required()
+});
+const recordSchema = Joi.object({
+  idRecord: Joi.string(),
+  description: Joi.string().required(),
+  value: Joi.string().required(),
+  status: Joi.string().valid("output", "input").required()
 });
 
 server.post("/register", async (req,res)=>{
@@ -108,43 +115,154 @@ server.post("/login", async(req,res)=>{
 });
 
 server.get("/dates", async(req,res)=>{
-  //receber token
-  //verificar se existe alguma sessao com esse token
-  //enviar dados do user correspondente e status 200
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+  try{
+    const sessionVerify = await db.collection("Sessions").findOne({token: token});
+    if(!sessionVerify){
+      res.sendStatus(404);
+      return;
+    }
+    const user = await db.collection("Users").findOne({_id: sessionVerify.idUser});
+    delete user._id;
+    delete user.password;
+    res.status(200).send(user);
+    
+  } catch(err){
+    res.status(500).send(err);
+  }
+  res.send(token);
+  //receber token                                     v
+  //verificar se existe alguma sessao com esse token  v
+  //enviar dados do user correspondente e status 200  v
 });
+
 server.post("/registration", async(req,res)=>{
-  //receber token, valor, descricao, status
-  //verificar se token existe 
-  //validar os dados recebidos
-  //buscar os dados do usuario
-  //gerar um id para o registro
-  //gerar uma data
-  //salvar o registro
-  //mandar para o cliente 201
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+  let record = req.body;
+
+  const validation = recordSchema.validate(record, { abortEarly: true });
+
+  if (validation.error) {
+    console.log(validation.error.details);
+    res.sendStatus(422);
+    return;
+  }
+  try{
+    const sessionVerify = await db.collection("Sessions").findOne({token: token});
+    if(!sessionVerify){
+      res.sendStatus(404);
+      return;
+    }
+    const user = await db.collection("Users").findOne({_id: sessionVerify.idUser});
+    record = {
+      idRecord: uuid(),
+      ...record,
+      day: dayjs().format("DD/MM")
+    };
+    user.records.push(record);
+    await db.collection("Users").updateOne({ 
+			_id: user._id 
+		}, { $set: user });
+    res.sendStatus(201);
+
+  }catch(err){
+    res.status(500).send(err);
+  }
+  //receber token, valor, descricao, status v
+  //verificar se token existe               v
+  //validar os dados recebidos              v
+  //buscar os dados do usuario              v
+  //gerar um id para o registro             v
+  //gerar uma data                          v
+  //salvar o registro                       v
+  //mandar para o cliente 201               v
 });
+
 server.delete("/deleteregister", async(req,res)=>{
-  //receber token e id do registro
-  //verificar se token existe 
-  //buscar usuario
-  //buscar registro
-  //verificar se existe
-  //deletar registro
-  //responder com 200
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+  let { idRecord } = req.body;
+  try{
+    const sessionVerify = await db.collection("Sessions").findOne({token: token});
+    if(!sessionVerify){
+      res.sendStatus(404);
+      return;
+    }
+    const user = await db.collection("Users").findOne({_id: sessionVerify.idUser});
+    const newRecords = user.records.filter((date)=> date.idRecord !== idRecord);
+    await db.collection("Users").updateOne({ 
+			_id: user._id 
+		}, { $set: {records: newRecords} });
+
+    res.sendStatus(200);
+  }catch(err){
+    res.status(500).send(err);
+  }
+  //receber token e id do registro  v
+  //verificar se token existe       v  
+  //buscar usuario                  v
+  //buscar registro                 v
+  //verificar se existe             v
+  //deletar registro                v
+  //responder com 200               v
 });
+
 server.delete("/endsession", async(req,res)=>{
-  //receber o token
-  //verificar se token existe
-  //delete token
-  //responde com 200
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+  try{
+    const sessionVerify = await db.collection("Sessions").findOne({token: token});
+    if(!sessionVerify){
+      res.sendStatus(404);
+      return;
+    }
+    await db.collection("Sessions").deleteOne({_id: sessionVerify._id});
+    res.sendStatus(200);
+  }catch(err){
+    res.status(500).send(err);
+  }
+  //receber o token             v
+  //verificar se token existe   v
+  //delete token                v
+  //responde com 200            v
 });
+
 server.put("/editregister", async(req,res)=>{
-  //receber o token, id do registro, valor, descricao e status
-  //verificar se o token existe
-  //validar os dados recebidos
-  //busca o usuaro
-  //busca o registro
-  //atualiza os dados do registro
-  //responde com status 200
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+  let recordUpdate = req.body;
+
+  const validation = recordSchema.validate(recordUpdate, { abortEarly: true });
+
+  if (validation.error) {
+    console.log(validation.error.details);
+    res.sendStatus(422);
+    return;
+  }
+  try{
+    const sessionVerify = await db.collection("Sessions").findOne({token: token});
+    if(!sessionVerify){
+      res.sendStatus(404);
+      return;
+    }
+
+    const user = await db.collection("Users").findOne({_id: sessionVerify.idUser});
+    const newRecords = user.records.map((date)=>{
+      if(date.idRecord === recordUpdate.idRecord){
+        date.value = recordUpdate.value;
+        date.description = recordUpdate.description;
+      }
+      return date;
+    });
+    await db.collection("Users").updateOne({ 
+			_id: user._id 
+		}, { $set: {records: newRecords} });
+    res.sendStatus(200);
+  }catch(err){
+    res.status(500).send(err);
+  }
 });
 
 
